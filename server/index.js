ff7fcbbc43fed4087afab89e2b2bfcdeeb77efda
Aniket-Shobhit <shobhitaniket3@gmail.com>
2023-07-29 20:46:51 +0530

    added dummy data

--- server/index.js
+++ server/index.js
@@ -14,7 +14,9 @@ import postRoutes from './routes/posts.js';
 import { register } from './controllers/auth.js';
 import { createPost } from './controllers/posts.js';
 import { verifyToken } from './middleware/auth.js';
-
+import User from './models/User.js';
+import Post from './models/Post.js';
+import { users, posts } from './data/index.js';
 
 // configurations
 const __filename = fileURLToPath(import.meta.url);
@@ -62,6 +64,10 @@ mongoose.connect(process.env.MONGO_URL, {
     app.listen(PORT,() => {
         console.log('App is running on server '+PORT);
     });
+
+    //dummy data, inserted only once at start 
+    // User.insertMany(users);
+    // Post.insertMany(posts);
 }).catch((e) => {
     console.log('Could not connect to database');
 });
