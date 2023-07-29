30dd62574cc6fb35d0217804485de025f1376fa8
Aniket-Shobhit <shobhitaniket3@gmail.com>
2023-07-29 20:22:05 +0530

    added routes

--- server/index.js
+++ server/index.js
@@ -9,10 +9,14 @@ import { fileURLToPath } from 'url'
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import authRoutes from './routes/auth.js';
+import userRoutes from './routes/users.js';
+import postRoutes from './routes/posts.js';
 import { register } from './controllers/auth.js';
+import { createPost } from './controllers/posts.js';
+import { verifyToken } from './middleware/auth.js';
+
 
 // configurations
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
@@ -26,8 +30,8 @@ app.use(bodyParser.urlencoded({ limit:'30mb', extended: true }));
 app.use(cors());
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
 
-// file storage
 
+// file storage
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
         cb(null, 'public/assets');
@@ -38,11 +42,16 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
+
 // routes with files
 app.post('/auth/register', upload.single('picture'), register);
+app.post('/posts', verifyToken, upload.single('picture'), createPost);
+
 
 // routes
 app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
+app.use('/posts', postRoutes);
 
 const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL, {
