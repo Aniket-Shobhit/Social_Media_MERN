a151a23e7c33001f68ac3a0fe83afdc4d47ce4c0
Aniket-Shobhit <shobhitaniket3@gmail.com>
2023-07-31 18:47:40 +0530

    .

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+// import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import authReducer from './state';
@@ -36,9 +36,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistStore(store)}>
-        <HashRouter>
+        {/* <HashRouter> */}
           <App />
-        </HashRouter>
+        {/* </HashRouter> */}
       </PersistGate>
     </Provider>
   </React.StrictMode>
