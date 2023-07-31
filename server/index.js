ef507cd6c2dad9a82e833acab743ad5d0ada984b
Aniket-Shobhit <shobhitaniket3@gmail.com>
2023-07-31 18:29:44 +0530

    .

--- server/index.js
+++ server/index.js
@@ -28,6 +28,12 @@ app.use(express.json());
 app.use(bodyParser.json({ limit: '30mb', extended: true}));
 app.use(bodyParser.urlencoded({ limit:'30mb', extended: true }));
 // app.use(cors());
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ["*"]);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 app.use(cors({
     origin:["http://localhost:3001","https://social-media-mern-7r25.onrender.com"]
 }));
